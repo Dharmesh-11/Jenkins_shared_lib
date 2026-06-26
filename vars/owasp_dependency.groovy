5c49504d1fdbd5bc17a1fac98200b02dbc17de53
Dharmesh Panpatil <dharmeshpanpatil01@gmail.com>
2026-06-26 19:24:30 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -3,7 +3,7 @@ def call() {
 
         dependencyCheck(
             odcInstallation: 'OWASP',
-            additionalArguments: "--scan ./ --nvdApiKey ${NVD_API_KEY}"
+            additionalArguments: "--scan ./ --nvdApiKey=${NVD_API_KEY} --nvdApiDelay 5000"
         )
 
         dependencyCheckPublisher(
